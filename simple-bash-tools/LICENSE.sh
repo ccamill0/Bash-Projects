d908b4198ddacf76ff8fcf93ba11236c45146568
Kayke <ccamillo.silva@usp.br>
2024-06-11 16:08:14 -0300

    Add files via upload

---
+++ simple-bash-tools/LICENSE.sh
@@ -0,0 +1,5 @@
+
+### Conclusão
+
+Este projeto fornece uma boa introdução ao Bash para iniciantes, cobrindo conceitos essenciais como loops, condicionais e manipulação de arquivos. Colocá-lo no GitHub oferece uma oportunidade para novos programadores colaborarem, aprenderem boas práticas de controle de versão e compartilharem seu trabalho com a comunidade.
+
